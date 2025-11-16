be3f5b3668bd478206f21fb8a35ee77ce01592ba
KoSi <otpog23@gmail.com>
2025-11-16 10:50:36 +0000

    Added: [Take Action page]

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home"
 import { Login } from "./pages/login"
 import { Signup } from "./pages/signup"
 import { Learn } from "./pages/Learn"
+import { TakeAction } from "./pages/TakeAction"
 
 
 export function App() {
@@ -37,6 +38,14 @@ export function App() {
       element: (
         <>
           <Learn/>
+        </>
+      )
+    },
+    {
+      path: '/takeaction',
+      element: (
+        <>
+          <TakeAction/>
         </>
       )
     },
