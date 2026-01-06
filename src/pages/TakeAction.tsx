827ae98f6708688fe6af26036e58aed44a4794f6
KoSi <otpog23@gmail.com>
2026-01-06 13:13:13 +0000

    Added:(TakeAction page) [Actions ]

--- src/pages/TakeAction.tsx
+++ src/pages/TakeAction.tsx
@@ -31,6 +31,44 @@ export function TakeAction() {
                 </div>
             </section>
 
+            <section id="actions" style={{height: '100vh'}}>
+                <div className="head">
+                    Small Act, Big Impact.
+                </div>
+
+                <div className="grid">
+                    <div id="action" className="recycle">
+                        <div className="img">
+                            <img src="/recycle-action.avif" />
+                        </div>
+
+                        <div className="txt">
+                            Don't be trashy! Recycle. Recycling isn’t just a habit, it’s a responsibility we all share to protect our planet for future generations.
+                        </div>
+                    </div>
+
+                    <div id="action" className="affor">
+                        <div className="img">
+                            <img src="/affor-action.avif" />
+                        </div>
+
+                        <div className="txt">
+                            Afforestation is more than planting trees—it’s planting hope, life, and a future for generations to come.
+                        </div>
+                    </div>
+
+                    <div id="action" className="save-energy">
+                        <div className="img">
+                            <img src="/save-energy-action.jpg" />
+                        </div>
+
+                        <div className="txt">
+                            Saving energy today means securing a cleaner, brighter, and more sustainable future for tomorrow.
+                        </div>
+                    </div>
+                </div>
+            </section>
+
             <div style={{marginTop: '50px'}}></div>
             
             <Footer/>
